14ff9496a8a43d5eeb1f2f6e06b0d7b2e3f345c8
Krzysztof Ragan <krzysztofragan1999@gmail.com>
2022-08-05 14:52:20 +0200

    added buttons with possibility to see folder with detection results

--- src/GUI.py
+++ src/GUI.py
@@ -3,6 +3,7 @@ import tkinter
 import customtkinter
 from tkinter import filedialog
 from tkinter import *
+import os, sys, glob
 
 
 customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
@@ -22,6 +23,21 @@ def detect_function():
     print("button pressed")
     subprocess.call(['python3.10', './yolov5/detect.py', '--source', detect_path, '--weights', './yolov5/runs/train/exp/weights/best.pt'])
 
+# def show_results():
+    # subprocess.Popen(r'./yolov5/runs/detect/exp2', shell=True)
+
+
+def show_results():
+    folder_list = os.listdir('./yolov5/runs/detect')
+    list_of_folders = glob.glob('./yolov5/runs/detect/*')
+    latest_folder = max(list_of_folders, key=os.path.getctime)
+
+    if sys.platform == "win32":
+        os.startfile(latest_folder)
+    else:
+        opener ="open" if sys.platform == "darwin" else "xdg-open"
+        subprocess.call([opener, latest_folder])
+
 detect_path = str()
 
 # Use CTkButton instead of tkinter Button
@@ -30,6 +46,9 @@ detect_button.place(relx=0.5, rely=0.2, anchor=tkinter.CENTER)
 
 choose_folder_button = customtkinter.CTkButton(master=app, text="WHAT DETECT?", command=choose_folder)
 choose_folder_button.place(relx=0.5, rely=0.4, anchor=tkinter.CENTER)
+
+show_results_button = customtkinter.CTkButton(master=app, text="SHOW RESULTS", command=show_results)
+show_results_button.place(relx=0.5, rely=0.6, anchor=tkinter.CENTER)
 
 app.mainloop()
 
