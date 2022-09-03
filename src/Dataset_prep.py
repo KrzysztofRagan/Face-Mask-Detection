2bc5846f478930286caebb6ddb27f9c5f3384705
Krzysztof Ragan <krzysztofragan1999@gmail.com>
2022-09-03 12:40:44 +0200

    Corrected directories with paths

--- src/Dataset_prep.py
+++ src/Dataset_prep.py
@@ -7,8 +7,8 @@ import os
 import shutil
 from random import choice
 
-img_folder = '/Users/krzysztofragan/Desktop/vscode/Face Mask Detection/archive/images/' #path to all images
-labels_folder = '/Users/krzysztofragan/Desktop/vscode/Face Mask Detection/archive/labels/' #path to all labels
+img_folder = '../archive/images/' #path to all images
+labels_folder = '../archive/labels/' #path to all labels
 
 train_factor = 0.8
 val_factor = 0.2
@@ -20,8 +20,8 @@ count_to_train = int(train_factor * len(all_img)) #how many images
 for i in range(count_to_train):
   img_file = choice(all_img) #random choice from images list
   label_file = img_file[:-4] + '.txt' #same choice for label
-  shutil.copy2(img_folder+img_file, '/Users/krzysztofragan/Desktop/vscode/Face Mask Detection/src/yolov5/dataset/images/train')
-  shutil.copy2(labels_folder+label_file, '/Users/krzysztofragan/Desktop/vscode/Face Mask Detection/src/yolov5/dataset/labels/train')
+  shutil.copy2(img_folder+img_file, '../ext/yolov5/dataset/images/train')
+  shutil.copy2(labels_folder+label_file, '../ext/yolov5/dataset/labels/train')
 
   all_img.remove(img_file)
   all_labels.remove(label_file)
@@ -29,8 +29,8 @@ for i in range(count_to_train):
 for i in range(len(all_img)):
   img_file = choice(all_img)
   label_file = img_file[:-4] + '.txt'
-  shutil.copy2(img_folder+img_file, '/Users/krzysztofragan/Desktop/vscode/Face Mask Detection/src/yolov5/dataset/images/val')
-  shutil.copy2(labels_folder+label_file, '/Users/krzysztofragan/Desktop/vscode/Face Mask Detection/src/yolov5/dataset/labels/val')
+  shutil.copy2(img_folder+img_file, '../ext/yolov5/dataset/images/val')
+  shutil.copy2(labels_folder+label_file, '../ext/yolov5/dataset/labels/val')
 
   all_img.remove(img_file)
   all_labels.remove(label_file)
--- src/functionality.py
+++ src/functionality.py
@@ -4,7 +4,7 @@ from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox as mbox
 
-detect_path = ('./yolov5/runs/train/exp/control')
+detect_path = ('../ext/yolov5/runs/train/exp/control')
 
 
 
@@ -21,14 +21,14 @@ def choose_folder():
 
 def detect_function():
   try:
-    subprocess.call(['python3.10', './yolov5/detect.py', '--source', detect_path, '--weights', './yolov5/runs/train/exp/weights/best.pt'])
+    subprocess.call(['python3.10', '../ext/yolov5/detect.py', '--source', detect_path, '--weights', '../ext/yolov5/runs/train/exp/weights/best.pt'])
     mbox.showinfo("Information", "Detection finished")
   except Exception as ex:
     print(f'Detection problem {ex}')
 
 def show_results():
-    folder_list = os.listdir('./yolov5/runs/detect')
-    list_of_folders = glob.glob('./yolov5/runs/detect/*')
+    folder_list = os.listdir('../ext/yolov5/runs/detect')
+    list_of_folders = glob.glob('../ext/yolov5/runs/detect/*')
     show_results.latest_folder = max(list_of_folders, key=os.path.getctime)
 
     if sys.platform == "win32":
